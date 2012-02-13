5397f3233e7fcd9b13e760e4e701a3fbe0853f77
Steve Crook <steve@mixmin.net>
2012-02-13 11:36:55 +0000

    Update GPL licenses and copyright years.

--- hsub.py
+++ hsub.py
@@ -2,12 +2,11 @@
 #
 # vim: tabstop=4 expandtab shiftwidth=4 autoindent
 #
-# Copyright (C) 2011 Steve Crook <steve@mixmin.net>
-# $Id$
+# Copyright (C) 2012 Steve Crook <steve@mixmin.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by the
-# Free Software Foundation; either version 2, or (at your option) any later
+# Free Software Foundation; either version 3, or (at your option) any later
 # version.
 #
 # This program is distributed in the hope that it will be useful, but
